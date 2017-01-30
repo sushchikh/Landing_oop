9cd5f534e95259d8bfc0bf41ff5d7f596b479255
a.sushchikh <a.sushchikh@gmail.com>
2017-01-30 21:28:13 +0300

    +carousel, +svg-logo, +svg-brands

--- js/my.js
+++ js/my.js
@@ -1,1 +1,37 @@
-$('.carousel').carousel()
+$(function(){
+
+
+
+var i = 1;
+
+
+$(".container").click(function(){
+		
+		switch (i){
+		case 1 :
+			$(".img_1").removeClass("show");
+			$(".img_2").addClass("show");
+			i = 2;
+			break;
+		
+		
+		case 2 :
+			$(".img_2").removeClass("show");
+			$(".img_3").addClass("show");
+			i = 3;
+			break;
+		
+
+		case 3 :
+			$(".img_3").removeClass("show");
+			$(".img_1").addClass("show");
+			i = 1;
+			break;
+		}
+		
+	})
+
+
+
+
+});
